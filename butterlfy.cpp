10cdc666b3828fa651e34f61266de0694f6cc930
nilesh2903 <nileshpalkar.study@gmail.com>
2024-04-21 05:09:27 +0530

    Successfully added the code for butterfly

--- butterlfy.cpp
+++ butterlfy.cpp
@@ -1,57 +1,46 @@
 #include <iostream>
 using namespace std;
 
-// Create an Function to Print an Output for n times
-void PrintNTimes(char c int i){
-    {
-        cout << string (i, c);
-    }
+void PrintNTimes(int i , char c)
+{
+    string (c, i);
+    cout << string (c, i);
 }
 
-//Craete an function to print an Empty Rever n times
-void RPrintNTimes(char d, int k){
-    {
-        cout << string (k, d);
-    }
-}
-// Print N Space for the butterfly pattern
-
-void PrintESpaces(char e, int l){
-    {
-        cout << string (l, e) ;
-    }
+void EmptyNTimes(int j, char d)
+{
+    string (d,j);
+    cout << string (d,j) ;
 }
 
 
-
-// Main Programme starts from here
 int main()
-{   
-    //Variable Declaration and Initialization
-    
-    int i,k,l, n=6 ; // Type the number of the times the character needs ti be printed
-    char c = 'X'; // Assign the character which need to be printed
-    char d = 'X'; // Assign the Character which needs to be printed
-    char e = 'Y'; // Assign the Caharacter for the Black Space 
-    cout << "The required output is" << endl ;
-
-    for (i=1; i<=n+1; i++){
-        {
-        int l= n-i;
-        // Function Calling 
-        PrintNTimes(c , i);
+{
+    int n;
+    int i,j;
+    char c= 'X';
+    char d= ' ';
+    cout << "Enter the Nummber:";
+    cin >> n;
+    for (i=1; i<=n+1; i++)
+    {
+        j= n+1-i;
+        PrintNTimes(c,i);
+        EmptyNTimes(d,j);
+        EmptyNTimes(d,j);
+        PrintNTimes(c,i);
         cout << endl;
     }
+    for (i=n; i>=1; i--)
+    {
+        j= n+1-i;
+        PrintNTimes(c,i);
+        EmptyNTimes(d,j);
+        EmptyNTimes(d,j);
+        PrintNTimes(c,i);
+        cout << endl;
     }
-    
-    for (k=n; k>=1;--k){
-        {
-        // Function Calling
-        int l= n-k;
-        RPrintNTimes(d, k);
-        }
-    }
-     
+
     return 0;
 
 }
